c150444f824dd5385bb6debb8e7fb33338825e26
voidkudo <voidkudo@gmail.com>
2024-08-29 01:58:18 -0400

    feat: error display

--- grocery-price-tracker-web/src/routers/pages/ErrorPage.tsx
+++ grocery-price-tracker-web/src/routers/pages/ErrorPage.tsx
@@ -1,13 +1,33 @@
-import { useRouteError } from 'react-router-dom';
+import { Container, Divider, Typography } from '@mui/material';
+import { useRouteError, isRouteErrorResponse, Link } from 'react-router-dom';
 
 const ErrorPage = () => {
-    const error = useRouteError();
+  const error = useRouteError();
 
-    return (
-      <>
-        <h1>Error Page</h1>
-      </>
-    )
+  const getErrorMessage = (error: unknown) => {
+    if (isRouteErrorResponse(error)) {
+      return `${error.status} ${error.statusText}`
+    } else if (error instanceof Error) {
+      return error.message
+    } else if (typeof error === 'string') {
+      return error
+    } else {
+      console.error(error)
+      return 'Unknown error'
+    }
   };
-  
-  export default ErrorPage;
+
+  return (
+    <Container>
+      <Typography variant='h1'>Oops!</Typography>
+      <Typography>Sorry, an unexpected error has occurred.</Typography>
+      <Typography>
+        <Typography variant='inherit'>{getErrorMessage(error)}</Typography>
+      </Typography>
+      <Divider />
+      <Typography>Click <Link to='/'>HERE</Link> to return to the home page.</Typography>
+    </Container>
+  )
+};
+
+export default ErrorPage;
