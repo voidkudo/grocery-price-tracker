7937cc41aac1206f4692e8633acd713c4c59611f
voidkudo <voidkudo@gmail.com>
2024-08-29 01:58:09 -0400

    feat: add font

--- grocery-price-tracker-web/src/App.tsx
+++ grocery-price-tracker-web/src/App.tsx
@@ -1,3 +1,8 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/router'
 import { Provider } from 'react-redux';
