b343a9ff18f148e78f6817e52ed5a15c37b85783
voidkudo <voidkudo@gmail.com>
2024-09-01 02:49:07 -0400

    refactor: clear page component

--- grocery-price-tracker-web/src/routers/pages/MainPage.tsx
+++ grocery-price-tracker-web/src/routers/pages/MainPage.tsx
@@ -1,47 +1,12 @@
-import { AppBar, Box, Button, Container, FormControl, InputAdornment, InputLabel, OutlinedInput, Toolbar, Typography } from "@mui/material"
-import SearchIcon from '@mui/icons-material/Search';
-import { useState } from "react";
+import { Box, Container } from "@mui/material"
+import NavBar from "../../components/NavBar";
 
 const MainPage = () => {
-  const [searchValue, setSearchValue] = useState('');
-
-  const handleSearch = () => {
-    console.log(searchValue);
-  };
-
-  const handleEnterKeyPress = (e: React.KeyboardEvent) => {
-    if (e.key === 'Enter') {
-      handleSearch();
-    }
-  };
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
-      <AppBar position='static'>
-        <Toolbar>
-          <Typography variant='h6' sx={{ flexGrow: 1 }}>Grocery Price Tracker</Typography>
-        </Toolbar>
-      </AppBar>
+      <NavBar />
       <Container maxWidth={false} sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <FormControl fullWidth variant='outlined'>
-          <InputLabel>Enter Product Name</InputLabel>
-          <OutlinedInput
-            endAdornment={
-              <InputAdornment position='end'>
-                <Button
-                  variant='contained'
-                  startIcon={<SearchIcon />}
-                  onClick={handleSearch}>
-                  SEARCH
-                </Button>
-              </InputAdornment>
-            }
-            label='Enter Product Name'
-            value={searchValue}
-            onKeyDown={handleEnterKeyPress}
-            onChange={(e) => {setSearchValue(e.target.value)}}
-          />
-        </FormControl>
       </Container>
     </Box>
   )
