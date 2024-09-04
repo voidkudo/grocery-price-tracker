a858f991a553a677bbfe334abad71ac78ec9f032
voidkudo <voidkudo@gmail.com>
2024-09-04 01:58:22 -0400

    fix: lint

--- grocery-price-tracker-web/src/stores/store.ts
+++ grocery-price-tracker-web/src/stores/store.ts
@@ -3,9 +3,9 @@ import userReducer from './slices/userSlice';
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
   },
-})
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
