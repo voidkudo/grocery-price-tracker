67abf121d9bc618499f0a9a89d72a9ccf60ba6a3
voidkudo <voidkudo@gmail.com>
2024-09-07 02:43:40 -0400

    feat: update router

--- grocery-price-tracker-web/src/routers/router.tsx
+++ grocery-price-tracker-web/src/routers/router.tsx
@@ -1,12 +1,23 @@
 import { createBrowserRouter } from 'react-router-dom';
-import MainPage from './pages/MainPage';
+import MainPage from './pages/layouts/MainLayout';
 import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import ItemPage from './pages/ItemPage';
 
 const router = createBrowserRouter([
   {
-    path: '/',
     element: <MainPage />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/item',
+        element: <ItemPage />,
+      }
+    ],
   },
 ]);
 
