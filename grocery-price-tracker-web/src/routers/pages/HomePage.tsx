1519cdffd0c1a16893924571d1da541edd890c54
voidkudo <voidkudo@gmail.com>
2024-09-11 03:04:40 -0400

    feat: implement category selection

--- grocery-price-tracker-web/src/routers/pages/HomePage.tsx
+++ grocery-price-tracker-web/src/routers/pages/HomePage.tsx
@@ -1,6 +1,43 @@
+import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2"
+import { useEffect, useState } from "react";
+import { getCategroies } from "../../data/data";
+import { createSearchParams, useNavigate } from "react-router-dom";
+import CardButton from "../../components/CardButton";
+
 const HomePage = () => {
+  const [categories, setCatecories] = useState<string[]>([]);
+
+  const navigate = useNavigate();
+
+  const handleCategroyClick = (category: string) => {
+    navigate({
+      pathname: '/category',
+      search: createSearchParams({
+        value: category
+      }).toString()
+    });
+  };
+
+  useEffect(() => {
+    getCategroies().then(data => setCatecories(data));
+  }, []);
+
   return (
-    <>Home Page</>
+    <Box sx={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}>
+      <Typography>Please Choose any Category</Typography>
+      <Grid container spacing={1} height={'80%'} width={'100%'} justifyContent={'center'} overflow={'auto'}>
+        {
+          categories.map((category, index) => {
+            return (
+              <Grid size={{ xs: 12, sm: 6 }} key={index}>
+                <CardButton handleClick={() => handleCategroyClick(category)}>{category}</CardButton>
+              </Grid>
+            )
+          })
+        }
+      </Grid>
+    </Box>
   )
 };
 
