421963b550f0f5eed43535d86d3fc1639089d096
voidkudo <voidkudo@gmail.com>
2024-09-11 03:00:31 -0400

    refactor: change data schema

--- grocery-price-tracker-web/src/data/data.ts
+++ grocery-price-tracker-web/src/data/data.ts
@@ -1,18 +1,29 @@
 import { GroceryItem } from "../types/data";
 
-
 export const getCategroies = async () => {
-  const mockData = await fetch('./mockData.json').then(res => res.json());
-  return mockData['categories'];
+  const categories = await fetch('./mockData.json')
+    .then(res => res.json())
+    .then(data => data['categories'] as string[]);
+  return categories;
 };
 
-export const getGroceryItemNames = async () => {
-  const mockData = await fetch('./mockData.json').then(res => res.json());
-  console.log(mockData)
-  return Object.keys(mockData['items']);
+export const getAllGroceryItemNames = async () => {
+  const items = await fetch('./mockData.json')
+    .then(res => res.json())
+    .then(data => data['items'] as GroceryItem[]);
+  return items.map(item => item.name);
+};
+
+export const getGroceryItemNamesByCategory = async (categoryName: string) => {
+  const items = await fetch('./mockData.json')
+    .then(res => res.json())
+    .then(data => data['items'] as GroceryItem[]);
+  return items.filter(item => item.category === categoryName).map(item => item.name);
 };
 
 export const getGroceryItemByName = async (itemName: string) => {
-  const mockData = await fetch('./mockData.json').then(res => res.json());
-  return { ...mockData['items'][itemName], name: itemName } as GroceryItem;
+  const items = await fetch('./mockData.json')
+    .then(res => res.json())
+    .then(data => data['items'] as GroceryItem[]);
+  return items.find(item => item.name === itemName);
 };
