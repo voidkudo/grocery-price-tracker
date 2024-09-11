0dadfc422643cc277b2cad3f1d6c5476821f326e
voidkudo <voidkudo@gmail.com>
2024-09-11 03:03:40 -0400

    fix: use height and width for root layout

--- grocery-price-tracker-web/src/routers/pages/layouts/MainLayout.tsx
+++ grocery-price-tracker-web/src/routers/pages/layouts/MainLayout.tsx
@@ -1,11 +1,11 @@
 import { Box, Container } from "@mui/material"
-import NavBar from "../../../components/NavBar";
+import NavBar from "../../../components/navBar/NavBar";
 import { Outlet } from "react-router-dom";
 
 const MainPage = () => {
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+    <Box sx={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column'}}>
       <NavBar />
       <Container sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'auto' }}>
         <Outlet />
