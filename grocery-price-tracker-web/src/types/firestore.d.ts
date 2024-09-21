43c4bd08f57194bdc27a32a17b8d346600f47d68
voidkudo <voidkudo@gmail.com>
2024-09-21 02:40:57 -0400

    feat: firestore doc type

--- grocery-price-tracker-web/src/types/firestore.d.ts
+++ grocery-price-tracker-web/src/types/firestore.d.ts
@@ -1,5 +1,14 @@
-export interface FireStoreMetadataOptions {
-  categories: string[],
+import { CollectionReference, Timestamp } from "firebase/firestore";
+
+export interface FireStoreDoc {
+  creationTimestamp: Timestamp,
+  createdBy: string,
+}
+
+export interface FireStoreCategoryDoc extends FireStoreDoc {
   items: string[],
-  stores: string[],
+}
+
+export interface FireStoreItemDoc extends FireStoreDoc {
+  category: string
 }
