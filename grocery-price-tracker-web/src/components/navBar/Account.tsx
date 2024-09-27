8d82bbb50ff90766ac25f466f67188b4c72b33dc
voidkudo <voidkudo@gmail.com>
2024-09-27 02:39:04 -0400

    refactor: remove not used button

--- grocery-price-tracker-web/src/components/navBar/Account.tsx
+++ grocery-price-tracker-web/src/components/navBar/Account.tsx
@@ -1,6 +1,6 @@
 import { Avatar, Box, Button, IconButton, ListItemIcon, Menu, MenuItem, Tooltip } from "@mui/material";
 import { useState } from "react";
-import { AccountCircle, Logout, Settings } from "@mui/icons-material";
+import { Logout } from "@mui/icons-material";
 import { User } from "firebase/auth";
 import GoogleIcon from '@mui/icons-material/Google';
 
@@ -42,18 +42,6 @@ const Account = (props: AccountProps) => {
 
       </Tooltip>
       <Menu anchorEl={anchorEl} open={isMenuOpen} onClose={handleMenuClose}>
-        <MenuItem>
-          <ListItemIcon>
-            <AccountCircle fontSize="small" />
-          </ListItemIcon>
-          Account
-        </MenuItem>
-        <MenuItem divider >
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Setting
-        </MenuItem>
         <MenuItem onClick={handleSignOutClick} >
           <ListItemIcon>
             <Logout fontSize="small" />
