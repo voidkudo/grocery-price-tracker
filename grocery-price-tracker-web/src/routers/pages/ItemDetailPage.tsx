67b1aac2be702644b38aa3df33d7e46c1f2846da
voidkudo <voidkudo@gmail.com>
2024-09-28 23:52:13 -0400

    feat: add price stat

--- grocery-price-tracker-web/src/routers/pages/ItemDetailPage.tsx
+++ grocery-price-tracker-web/src/routers/pages/ItemDetailPage.tsx
@@ -7,12 +7,16 @@ import { getItemDetailByName } from "../../firebase/firestore";
 import { navigateToItemPage } from "../navigate";
 import { FireStoreItemDetailDoc } from "../../types/firestore";
 import PriceRecordCard from "../../components/PriceRecordCard";
+import PriceCard from "../../components/PriceCard";
+import { getAveragePrice, getMaxPrice, getMinPrice } from "../../commons/priceStat";
 
 const ItemDetailPage = () => {
   const [itemDetail, setItemDetail] = useState<FireStoreItemDetailDoc>();
 
   const itemDetailValue = useParam();
   const navigate = useNavigate();
+
+  const unitPriceRecords = itemDetail?.priceRecords.map(priceRecord => priceRecord.price / priceRecord.qty);
 
   useEffect(() => {
     if (itemDetailValue === undefined) {
@@ -41,12 +45,23 @@ const ItemDetailPage = () => {
                 <Chip label={itemDetail.item} variant='outlined' clickable onClick={() => navigateToItemPage(navigate, itemDetail.item)} />
               </Box>
             </Box>
-
             <Grid container spacing={1} height={'80%'} width={'100%'} justifyContent={'center'} alignItems={'center'} alignContent={'start'} overflow={'auto'}>
-              <Grid size={{ xs: 12, sm: 6 }}>
+              <Grid container spacing={1} size={{ xs: 12, sm: 6 }} alignSelf={'start'} justifyContent={'center'} alignItems={'center'} >
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <PriceCard label='Lowest Price' price={getMinPrice(unitPriceRecords!)} backgroundColor='warning.dark' />
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <PriceCard label='Highest Price' price={getMaxPrice(unitPriceRecords!)} backgroundColor='error.dark' />
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <PriceCard label='All Time Average Price' price={getAveragePrice(unitPriceRecords!)} backgroundColor='success.dark' />
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <PriceCard label='Last 10 Average Price' price={getAveragePrice(unitPriceRecords!.slice(-10))} backgroundColor='info.dark' />
+                </Grid>
               </Grid>
-              <Grid size={{ xs: 12, sm: 6 }}>
-                <Stack spacing={1} width={'100%'} height={'100%'} overflow={'auto'}>
+              <Grid size={{ xs: 12, sm: 6 }} alignSelf={'start'}>
+                <Stack spacing={1} width={'100%'} height={'100%'}>
                   {
                     itemDetail.priceRecords.map((priceRecord, index) => (
                       <PriceRecordCard
